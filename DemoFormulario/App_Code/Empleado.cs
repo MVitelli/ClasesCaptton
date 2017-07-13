63375033fafd412c085be902de342be39604fb33
MVitelli <movitelli@gmail.com>
2017-07-13 17:43:35 -0300

    Ejercicios y Demo Dia 09
    Ejercicios y demostracion del dia 09, con scripts de bases de datos

---
+++ DemoFormulario/App_Code/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Empleado
+/// </summary>
+public class Empleado
+{
+    public string nombre, apellido, area;
+    public int dni;
+    public Empleado(string name, string lastname, string area, int dni)
+    {
+        this.apellido = lastname;
+        this.nombre = name;
+        this.dni = dni;
+        this.area = area;
+    }
+}
